89717b93a2defa2afc85bd041b74668a1fa6f8ae
KamSaf <bopaf42@gmail.com>
2024-02-13 21:45:30 +0100

    Added encore, node, jquery and javascript code for user profile url template, fixed footer

---
+++ assets/javascript/profile.js
@@ -0,0 +1,45 @@
+$(function() {
+    $(".delete-profile-button").on("click", function() {
+      var modal = new bootstrap.Modal(document.getElementById("confirm_delete_modal"));
+      modal.show();
+    });
+
+    $(".confirm-delete-profile-button").on("click", function() {
+      var userId = $(this).data("userId");
+
+    //   $.ajax({
+    //     type: "POST",
+    //     url: "",
+    //     data: {
+    //       postId: postId
+    //     },
+    //     success: function(response) {
+    //       $("#post_" + postId).remove();
+    //       modal.hide();
+    //     }
+    //   });
+    });
+});
+
+$(function() {
+    $(".edit-profile-button").on("click", function() {
+      var modal = new bootstrap.Modal(document.getElementById("edit_profile_modal"));
+      modal.show();
+    });
+
+    $(".save-edit-profile-button").on("click", function() {
+      var userId = $(this).data("userId");
+
+    //   $.ajax({
+    //     type: "POST",
+    //     url: "",
+    //     data: {
+    //       postId: postId
+    //     },
+    //     success: function(response) {
+    //       $("#post_" + postId).remove();
+    //       modal.hide();
+    //     }
+    //   });
+    });
+});
